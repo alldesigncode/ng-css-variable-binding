f87a2b57209de5963d38e9ef9096df542f217999
Beka Maisuradze <bekamais@Bekas-MacBook-Pro.local>
2021-03-11 16:20:53 +0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ng-css-variable-binding';
+export class AppComponent implements OnInit {
+  @ViewChild('nav', { static: true }) nav: ElementRef<HTMLDivElement>;
+
+  public navList = [
+    { name: 'Home' },
+    { name: 'Features' },
+    { name: 'Contact' },
+    { name: 'About' },
+  ];
+
+  navWidth = 500;
+  lineWidth = 0;
+  translateLine = 0;
+
+  ngOnInit() {
+    this.lineWidth =
+      Math.floor(this.navWidth / this.navList.length) ??
+      Math.floor(
+        this.nav.nativeElement.getBoundingClientRect().width /
+          this.navList.length
+      );
+  }
+
+  onClick(item: HTMLDivElement, i: number) {
+    const { width } = item.getBoundingClientRect();
+    this.translateLine = width * i;
+  }
 }
